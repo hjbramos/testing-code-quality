e41ef946ab40a70e1076c62fab4220fc2572d290
Helder Ramos <hjbramos@gmail.com>
2018-02-28 11:10:09 -0400

    Added mocha config

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,4 +1,6 @@
-const assert = require('assert');
+const chai = require('chai');
+const assert = chai.assert;
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
